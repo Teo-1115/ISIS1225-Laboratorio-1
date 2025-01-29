9300e207314d01b2b902cd27d2463a16a376dfcf
Teo_1115 <m.ojedah@uniandes.edu.co>
2025-01-29 10:11:41 -0500

    modificaciones individuales laboratorio 1.

--- App/logic.py
+++ App/logic.py
@@ -135,12 +135,17 @@ def load_books_tags(catalog, filename):
     :returns: Tamaño del conjunto de tags de los libros
     :rtype: int
     """
-    # TODO: Mods Lab 1, integrar vista y logica
-    # Implemente una función que cargue los tags de los libros en el catalogo.
-    # La función debe recibir el catalogo y el nombre del archivo csv con los tags de los libros.
-    # La función debe cargar los tags de los libros del archivo y los agregar al conjunto book_tags del catalogo.
-    # La función debe retornar el tamaño del conjunto de tags de los libros.
-    pass
+    book_tags = catalog.get("Books_tags")
+    book_tags_file = os.path.join(data_dir, filename)
+    catalog ["Book_tags"] = set.load_set(book_tags, book_tags_file)
+    
+    if book_tags is None: 
+        return None
+    else: 
+        return set.size(catalog.get("Book_tags"))
+    
+    
+   
 
 # Funciones de consulta
 
